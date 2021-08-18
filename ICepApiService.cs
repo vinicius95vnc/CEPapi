2cc2f961c3c5120d84299ec0b088419f5177f75e
Vinicius95vnc <xvncxx@gmail.com>
2021-08-18 16:40:46 -0300

    Adicionar arquivos de projeto.

---
+++ ICepApiService.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace api1
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Refit;
+
+namespace api1
+{
+    class MainClass
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                Console.WriteLine("Informe seu Cep:");
+
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando informações do CEP: " + cepInformado);
+
+                var address = await cepClient.GetAddressAsync(cepInformado);
+
+                Console.Write($"\nLogradouro:{address.Logradouro},\nBairro:{address.Bairro},\nCidade:{address.Localidade}");
+                Console.ReadKey();
+
+            } catch (Exception e) 
+            {
+                Console.WriteLine("Erro na consulta de cep: " + e.Message);
+            }
+            
+        }
+    }
+}
